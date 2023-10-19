f563449e3c3abdf2dfd903d54ca733510e166451
Pro <62343019+bitpez@users.noreply.github.com>
2023-10-18 19:00:06 -0500

    Initial commit

---
+++ Class2/Commands.sql
@@ -0,0 +1,5 @@
+/* https://www.w3schools.com/sql/trysql.asp?filename=trysql_editor */
+-- Other comment
+
+-- 1 SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate FROM Orders INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+-- 2 CREATE INDEX idx_find ON Products (ProductName); (column1, column2, ...);
---
+++ Class2/Start.sql
@@ -0,0 +1,11 @@
+/* https://www.w3schools.com/sql/trysql.asp?filename=trysql_editor */
+-- Other comment
+SELECT * FROM Customers;
+
+-- 1 SELECT CustomerName, City FROM Customers;
+
+-- 2 SELECT DISTINCT Country FROM Customers; This statement is used to return only distinct (different) values.
+
+-- 3 SELECT * FROM Customers WHERE Country='Mexico'; It is used to extract only those records that fulfill a specified condition.
+
+-- 4 SELECT * FROM Products ORDER BY Price; ASC|DESC; This keyword is used to sort the result-set in ascending or descending order.
